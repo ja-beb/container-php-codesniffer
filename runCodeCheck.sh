ed7b997ce543f00ff9a264febf5e64dd32f5a252
Sean Bourg <sean.bourg@gmail.com>
2021-10-11 20:42:48 -0500

    moved code directory from /tmp/src to /tmp

--- runCodeCheck.sh
+++ runCodeCheck.sh
@@ -2,6 +2,6 @@
 # Run code sniffer (phpcs) and repair (phpbf)
 
 echo "[run.sh]::codesniffer start"
-/usr/local/bin/phpcs --standard=/usr/local/etc/phpCodeSniffer/php-rules.xml /tmp/src
-/usr/local/bin/phpcbf --standard=/usr/local/etc/phpCodeSniffer/php-rules.xml /tmp/src
+/usr/local/bin/phpcs --standard=/usr/local/etc/phpCodeSniffer/php-rules.xml /tmp
+/usr/local/bin/phpcbf --standard=/usr/local/etc/phpCodeSniffer/php-rules.xml /tmp
 echo "[run.sh]::codesniffer completed"
